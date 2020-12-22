9c3841281783853c08c252b433d17a8794e83416
Zixuan Chen <zchen4016@gmail.com>
2020-12-22 14:56:32 +0800

    Create util.py

---
+++ util.py
@@ -0,0 +1,30 @@
+import numpy as np
+import numpy.linalg as LA
+
+
+def weight_analysis(w1, w2):
+    rela_transpose = LA.norm(np.transpose(w1) - w2)
+    print("transpose relationship: ", rela_transpose)
+    rela_inverse = LA.norm(LA.pinv(w1) - w2)
+    print("inverse relationship: ", rela_inverse)
+
+
+def try_construct():
+    nr, nc = 128, 128
+
+    x = np.random.rand(nc, 1)
+    A = np.random.rand(nr, nc)
+
+    y = np.matmul(A, x)
+
+    Ai = np.linalg.pinv(A)
+    xr = np.matmul(Ai, y)
+
+    print(np.linalg.norm(xr-x))
+
+
+if __name__=='__main__':
+    for _ in range(100):
+        try_construct()
+
+
